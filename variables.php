0964c45d93b3c9eacda7b7fbb4c8225aa3b24e1a
TheGreekCuban <theodorealexander92@gmaill.com>
2020-01-25 15:04:10 -0500

    added variables lesson

--- variables.php
+++ variables.php
@@ -7,6 +7,19 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+        
+        $characterName = John;
+        $characterAge = 22;
+
+        echo "There was a man named $characterName <br>";
+        echo "He was $characterAge years old <br>";
+        //You can reassign the variable and it will keep the initial name and then after the reassignment it will change. Variables are simply containers of data - like in js.
+
+        $characterName = Mike;
+        echo "He really liked the name $characterName <br>";
+        echo "He didn't like being $characterAge";
+    ?>
+
 </body>
 </html>
