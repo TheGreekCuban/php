526c180b83daccbff13b040aeba4fa2bd5b772bd
TheGreekCuban <theodorealexander92@gmaill.com>
2020-01-25 15:14:55 -0500

    added data types

---
+++ datatypes.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Data Types</title>
+</head>
+<body>
+
+    <?php
+        
+        //String
+        $myString = "Hello thiis is a string";
+
+        //Numbers
+
+        //Integer -> Whole number (negative or positive)
+        $age = 4;
+
+        //Floating Point (Floats) -> Decimal point number. These are treated differently than integers.
+        $gpa = 3.45;
+
+        //Boolean -> binary data type. 
+        $isMale = true;
+
+        //Null -> stands for no value
+        $noValue = null;
+
+        echo $isMale;
+    
+    ?>
+
+    
+</body>
+</html>
